d62bce80b3db6a2041ceafce44532b6221bf24b8
cosminbordianu <cosminbordianu3@gmail.com>
2025-05-10 10:06:10 +0300

    test: verify public access using Netlify serverless

---
+++ functions/fetch-weather/fetch-weather.mjs
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+const handler = async (event) => {
+  const { lat, lon } = event.queryStringParameters;
+
+  const API_SECRET = process.env.API_SECRET;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_SECRET}`;
+
+  try {
+    const { data } = await axios.get(url);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response;
+    return {
+      statusCode: status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    };
+  }
+};
+
+module.exports = { handler };
